3c5c28a198b28b8fe31c41254bc58ba559a140f0
varshinipriya495-stack <varshinipriya495@gmail.com>
2026-09-17 11:37:06 +0530

    Add week number to weekday converter

---
+++ week_number_to_weekday.py
@@ -0,0 +1,18 @@
+week = int(input("Enter week number (1-7): "))
+
+if week == 1:
+    print("Monday")
+elif week == 2:
+    print("Tuesday")
+elif week == 3:
+    print("Wednesday")
+elif week == 4:
+    print("Thursday")
+elif week == 5:
+    print("Friday")
+elif week == 6:
+    print("Saturday")
+elif week == 7:
+    print("Sunday")
+else:
+    print("Invalid week number")

c5bf178a33af97f164c5af0d9abd6661bb9fde2a
varshinipriya495-stack <varshinipriya495@gmail.com>
2026-09-18 18:14:16 +0530

    Add prime number finder from 1 to n
    This script prompts the user for a number n and prints all prime numbers from 1 to n.

---
+++ prime_numbers_1_to_n.py
@@ -0,0 +1,11 @@
+n = int(input("Enter n: "))
+
+for num in range(2, n + 1):
+    count = 0
+
+    for i in range(1, num + 1):
+        if num % i == 0:
+            count += 1
+
+    if count == 2:
+        print(num)

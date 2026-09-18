6e8bc892f3714a0ba861d344c5f56b264f3f6093
varshinipriya495-stack <varshinipriya495@gmail.com>
2026-09-18 18:15:10 +0530

    Add function to calculate sum of prime numbers

---
+++ sum_of_prime_numbers.py
@@ -0,0 +1,15 @@
+n = int(input("Enter n: "))
+
+total = 0
+
+for num in range(2, n + 1):
+    count = 0
+
+    for i in range(1, num + 1):
+        if num % i == 0:
+            count += 1
+
+    if count == 2:
+        total = total + num
+
+print("Sum of prime numbers =", total)

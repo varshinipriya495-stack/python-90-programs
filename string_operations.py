f8d503c4612d962eee51de2363bec1f743988a82
Varshini Priya <varshinipriya495@gmail.com>
2026-09-19 19:40:55 +0530

    Add string operations for input, length, and comparison

---
+++ string_operations.py
@@ -0,0 +1,12 @@
+str1 = input("Enter first string: ")
+str2 = input("Enter second string: ")
+
+print("Length of first string =", len(str1))
+print("Length of second string =", len(str2))
+
+if str1 == str2:
+    print("Strings are equal")
+else:
+    print("Strings are not equal")
+
+print("Concatenated string =", str1 + str2)
